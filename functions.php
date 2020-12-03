6a61dd09de10d4db27ca4e898fcd9a591357f8da
Krishna Kant <lushkant@gmail.com>
2020-12-03 18:55:53 +0530

    Add initial files

---
+++ functions.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Carbon Child Theme
+ *
+ * Place any custom functionality/code snippets here.
+ *
+ * @since Carbon Child 1.0
+ */
+
+/**
+ * Include child theme styles.
+ *
+ * @return void
+ */
+function carbon_child_styles() {
+	$theme = wp_get_theme();
+	wp_enqueue_style( 'carbon-style', get_template_directory_uri() . '/style.css', array(), $theme->parent()->get( 'Version' ) );
+	wp_enqueue_style( 'carbon-child-style', get_stylesheet_uri(), array( 'blink-style' ), $theme->get( 'Version' ) );
+}
+add_action( 'wp_enqueue_scripts', 'carbon_child_styles' );
---
+++ readme.txt
@@ -0,0 +1,3 @@
+# Carbon Child Theme
+
+Add custom CSS to style.css and custom PHP functionality to functions.php
