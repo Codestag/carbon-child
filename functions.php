72aacf041cfa1530f5edcbc0e4aa79bdd2448b9b
Krishna Kant <lushkant@gmail.com>
2020-12-03 19:08:44 +0530

    Fix typo

--- functions.php
+++ functions.php
@@ -15,6 +15,6 @@
 function carbon_child_styles() {
 	$theme = wp_get_theme();
 	wp_enqueue_style( 'carbon-style', get_template_directory_uri() . '/style.css', array(), $theme->parent()->get( 'Version' ) );
-	wp_enqueue_style( 'carbon-child-style', get_stylesheet_uri(), array( 'blink-style' ), $theme->get( 'Version' ) );
+	wp_enqueue_style( 'carbon-child-style', get_stylesheet_uri(), array( 'carbon-style' ), $theme->get( 'Version' ) );
 }
 add_action( 'wp_enqueue_scripts', 'carbon_child_styles' );
